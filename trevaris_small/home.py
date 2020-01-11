7a4b0d073a57f4176b03cef63d2cc47f0e3be70c
azraelgnosis <azraelgnosis@gmail.com>
2020-01-11 16:46:09 -0500

    Add navbar to base.html

--- trevaris_small/home.py
+++ trevaris_small/home.py
@@ -4,6 +4,6 @@ from flask import (
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
-@bp.route('/', methods='GET')
+@bp.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
