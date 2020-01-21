05babb8de347671dce855155e73a0f2ab42d8b88
azraelgnosis <azraelgnosis@gmail.com>
2020-01-20 21:57:08 -0500

    Fix stylesheet not found error

--- trevaris_small/__init__.py
+++ trevaris_small/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY = 'dev',
     )
+    app.static_folder = 'static'
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
