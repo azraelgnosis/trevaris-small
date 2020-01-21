22d48efb61556aec85b7e537e1ba0a3aa3f5b04f
azraelgnosis <azraelgnosis@gmail.com>
2020-01-20 21:16:21 -0500

    Make projects list iterable

--- trevaris_small/home.py
+++ trevaris_small/home.py
@@ -6,4 +6,16 @@ bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    projects = get_projects()
+    return render_template('index.html', projects=projects)
+
+def get_projects():
+    import json
+    import os
+
+    path = os.path.dirname(__file__)
+    
+    with open(f"{path}/projects.json", "r") as f:
+        projects = json.load(f)
+    
+    return projects
